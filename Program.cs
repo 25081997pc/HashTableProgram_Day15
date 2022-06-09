8369f5670268798ea2360d645384a0cd296af97d
25081997pc <chavanpooja2508@gmail.com>
2022-06-09 18:49:24 +0530

    message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Hashtable_Day15
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+
+            Frequency frequency = new Frequency();
+
+            Console.WriteLine("Frequency of Words: ");
+            string str = "to be or not to be";
+            frequency.count_frequency(str);
+           
+        }
+    }
+}
