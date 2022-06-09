040daca1e525a6306bc2918311dc69bbe480c566
25081997pc <chavanpooja2508@gmail.com>
2022-06-09 18:51:26 +0530

    Counting Frequency of words

---
+++ Frequency.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Hashtable_Day15
+{
+    public class Frequency
+    {  
+        public void count_frequency(string sentence)
+        {
+            Hashtable MyMapNode = new Hashtable();
+            string[] arr = sentence.Split();
+
+            for(int i = 0; i< arr.Length; i++)
+            {
+                if (MyMapNode.ContainsKey(arr[i]))
+                {
+                    MyMapNode[arr[i]] = (int)MyMapNode[arr[i]] + 1;
+                }
+                else
+                {
+                    MyMapNode.Add(arr[i], 1);
+                }
+            }
+            foreach(DictionaryEntry element in MyMapNode)
+            {
+                Console.WriteLine(element.Key + " " + element.Value);
+            }
+        }
+    }
+}
