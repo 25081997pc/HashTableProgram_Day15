722ad6386796066a4ade4b695b5582450055e030
25081997pc <chavanpooja2508@gmail.com>
2022-06-09 19:07:10 +0530

    Adding Frequency object

--- Program.cs
+++ Program.cs
@@ -8,11 +8,15 @@
 
             Frequency frequency = new Frequency();
 
+            //Console.WriteLine("Frequency of Words: ");
+
+            //string str = "to be or not to be";
+            //frequency.count_frequency(str);
+
             Console.WriteLine("Frequency of Words: ");
 
-            string str = "to be or not to be";
-            frequency.count_frequency(str);
-           
+            string string2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            frequency.count_frequency(string2);
         }
     }
 }
